02b0190df7e12b40254c4d67b6853161dfea7dd8
NicatAlaskarli <154456178+NicatAlaskarli@users.noreply.github.com>
2026-02-12 09:44:59 +0400

    Add files via upload

--- Lecture.java
+++ Lecture.java
@@ -1,8 +1,11 @@
-package week04;
+package week04.Invoice;
 
 public class Lecture {
-   public static void main(String[] args){
-      Segment s = new Segment(3.14f, 1.1f, 2.0f, 3.0f);
-      System.out.println("x1p1_x = " + s.getP1().getX());
+   public static void main(String[] args0) {
+      Invoice invoice = new Invoice("1234", "Widget", 10, 2.5);
+      System.out.println("Part Number: " + invoice.getPartNumber());
+      System.out.println("Part Description: " + invoice.getPartDescription());
+      System.out.println("Quantity: " + invoice.getQuantity());
+      System.out.println("Price per Item: " + invoice.getPricePerItem());
    }
 }
